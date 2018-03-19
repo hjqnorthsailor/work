303938ebfbbc3887aaab51581a33d7c57c820456
hjqnorthsailor <hjqnorthsailor@gmail.com>
2018-03-19 13:48:13 +0800

    新增标签页的Dao

---
+++ src/main/java/com/tmooc/work/dao/TabDao.java
@@ -0,0 +1,7 @@
+package com.tmooc.work.dao;
+
+import com.tmooc.work.entity.Tab;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TabDao extends JpaRepository<Tab,Integer> {
+}
